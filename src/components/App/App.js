7201963ad1fefbf78e96506ce13dc21b25bd25ee
yeg2799 <yeg2799@gmail.com>
2021-01-04 16:14:23 +0300

    design

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,26 @@
-
-import React from 'react'
-import './style/App/app.scss'
+import React from "react";
+import Header from "../Header/Header";
+import "../../style/App/app.scss";
 const App = () => {
   return (
     <>
-    
+      <div className="container">
+        <div className="row">
+          <Header />
+        </div>
+        <div className="row">
+          Menü
+        </div>
+        <div className="row">
+          <div className="col-8">Content</div>
+          <div className="col-4">Hakkımda</div>
+        </div>
+        <div className="row">
+         Footer
+        </div>
+      </div>
+     
     </>
-  )
-}
-export default App
-
+  );
+};
+export default App;
