d5d488e1d4a9349ef02d6f7383143d536a880ae3
yeg2799 <yeg2799@gmail.com>
2021-02-18 23:55:41 +0300

    menu bazı kısımlar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,13 +4,14 @@ import { Col, Container, Row } from 'reactstrap';
 import Menu from "../Menu/Menu";
 import HomePage from "../Content/HomePage";
 import Content from "../Content/Content";
+
 const App = () => {
   return (
     <React.Fragment>
       
-        <Row>
-          <Col xs="2"><Menu/></Col>
-          <Col xs="10"><Content></Content></Col>
+        <Row style={{width:"100%"}}>
+          <Col xs="3"><Menu/></Col>
+          <Col xs="9"><Content></Content></Col>
         </Row>
       
     </React.Fragment>
--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,16 +1,48 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button, Row } from "reactstrap";
+import "../../style/Menu/menu.scss";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { AiOutlineHome } from "react-icons/ai";
+import profilePicture from "../../images/profile.jpg";
 const Menu = () => {
+  const sliderMenu = () => {
     return (
-        <div>
-            <Link to="/">Anasayfa</Link>
-            <Link to="/hakkimda">Hakkımda</Link>
-            <Link to="/projelerim">Projelerim</Link>
-            <Link to="/blog">Blog</Link>
-            <Link to="/iletisim">İletişim</Link>
-        </div>
-    )
-}
+      <div className="menu">
+        <Row>
+          <img src={profilePicture} alt="profilresmi"></img>
+        </Row>
+        <Row className="menu-links">
+          <Button className="menu-button">
+            <AiOutlineHome style={{color:"#13285E"}}/>
+            <Link to="/" style={{color:"#13285E",textDecoration:"none"}}>Anasayfa</Link>
+          </Button>
+          <Button className="menu-button">
+            <Link to="/hakkimda" style={{color:"#13285E",textDecoration:"none"}}>Hakkımda</Link>
+          </Button>
+          <Button className="menu-button">
+            <Link to="/projelerim" style={{color:"#13285E",textDecoration:"none"}}>Projelerim</Link>
+          </Button>
+          <Button className="menu-button">
+            <Link to="/blog" style={{color:"#13285E",textDecoration:"none"}}>Blog</Link>
+          </Button>
+          <Button className="menu-button">
+            <Link to="/iletisim" style={{color:"#13285E",textDecoration:"none"}}>İletişim</Link>
+          </Button>
+        </Row>
 
-export default Menu
+        <Row>sosyal media hesapları</Row>
+      </div>
+    );
+  };
+  return (
+    <React.Fragment>
+      {sliderMenu()}
+      <div className="menu-hamburger">
+        <GiHamburgerMenu onSubmit={sliderMenu()} />
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Menu;
