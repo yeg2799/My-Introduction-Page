582e26e4f15d6fb72477bf8c16ea56cd39b19972
yeg2799 <yeg2799@gmail.com>
2021-02-19 22:37:22 +0300

    iletişim kısmı oluşturuldu

--- src/components/Content/Contact.js
+++ src/components/Content/Contact.js
@@ -1,9 +1,34 @@
 import React from 'react'
-
+import { Col, Row, Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import {BsFillPersonFill} from 'react-icons/bs'
 const Contact = () => {
     return (
-        <div >
-            İletişim
+        <div style={{paddingTop:"50px"}}>
+           <h1 style={{textAlign:"center"}}>İletişim</h1> 
+           <Form style={{width:"50%",margin:"50px auto"}}>
+      <Row form >
+        <Col md={6}>
+          <FormGroup>
+            <Input type="text"  placeholder="&#x212B;Adınız" />
+            
+          </FormGroup>
+        </Col>
+        <Col md={6}>
+          <FormGroup>
+            <Input type="text"  placeholder="Soyadınız" />
+          </FormGroup>
+        </Col>
+      </Row>
+      <FormGroup>
+        <Input type="text"  placeholder="Email"/>
+      </FormGroup>
+      <FormGroup>
+        <Input type="textarea"  placeholder="Adresinizi giriniz" style={{height:"200px"}}/>
+      </FormGroup>
+     
+     
+      <Button style={{width:"100%"}}>Sign in</Button>
+    </Form>
         </div>
     )
 }
