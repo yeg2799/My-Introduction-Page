94e2355737fd5f8d0eeb7f5dfd9ad5be80f7cb2c
yeg2799 <yeg2799@gmail.com>
2021-02-19 22:00:27 +0300

    projelerim kısmı halledildi

--- src/components/Content/MyProject.js
+++ src/components/Content/MyProject.js
@@ -1,11 +1,74 @@
-import React from 'react'
-
+import React from "react";
+import {
+  Card,
+  CardImg,
+  CardTitle,
+  Row,
+} from "reactstrap";
+import projectImage from '../../images/projectImage.jpg'
 const MyProject = () => {
-    return (
-        <div>
-            Projelerim
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1 style={{textAlign:"center"}}>Projelerim</h1>
+      <Row style={{margin:"0",padding:"50px"}}>
+      <Card style={{width:"30%",margin:"20px",borderRadius:"20px"}}>
+      <CardTitle tag="h3" style={{textAlign:"center"}}>Card title</CardTitle>
+        <CardImg
+          top
+          width="100%"
+          src={projectImage}
+          alt="Card image cap"
+        />
+      </Card>
+      <Card style={{width:"30%",margin:"20px",borderRadius:"20px"}}>
+      <CardTitle tag="h3" style={{textAlign:"center"}}>Card title</CardTitle>
+        <CardImg
+          top
+          width="100%"
+          src={projectImage}
+          alt="Card image cap"
+        />
+      </Card>
+      <Card style={{width:"30%",margin:"20px",borderRadius:"20px"}}>
+      <CardTitle tag="h3" style={{textAlign:"center"}}>Card title</CardTitle>
+        <CardImg
+          top
+          width="100%"
+          src={projectImage}
+          alt="Card image cap"
+        />
+      </Card>
+      <Card style={{width:"30%",margin:"20px",borderRadius:"20px"}}>
+      <CardTitle tag="h3" style={{textAlign:"center"}}>Card title</CardTitle>
+        <CardImg
+          top
+          width="100%"
+          src={projectImage}
+          alt="Card image cap"
+        />
+      </Card>
+      <Card style={{width:"30%",margin:"20px",borderRadius:"20px"}}>
+      <CardTitle tag="h3" style={{textAlign:"center"}}>Card title</CardTitle>
+        <CardImg
+          top
+          width="100%"
+          src={projectImage}
+          alt="Card image cap"
+        />
+      </Card>
+      <Card style={{width:"30%",margin:"20px",borderRadius:"20px"}}>
+      <CardTitle tag="h3" style={{textAlign:"center"}}>Card title</CardTitle>
+        <CardImg
+          top
+          width="100%"
+          src={projectImage}
+          alt="Card image cap"
+        />
+      </Card>
+      </Row>
+      
+    </div>
+  );
+};
 
-export default MyProject
+export default MyProject;
