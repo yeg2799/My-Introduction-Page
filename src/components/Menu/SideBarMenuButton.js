70a19d3f7165d101e699510568a35319420b511d
yeg2799 <yeg2799@gmail.com>
2021-03-03 00:36:21 +0300

    size configs

--- src/components/Menu/SideBarMenuButton.js
+++ src/components/Menu/SideBarMenuButton.js
@@ -1,14 +1,11 @@
 
 import React from 'react'
-
 import { Link } from 'react-router-dom'
-
-
 const SideBarMenuButton = ({Icon,to,title}) => {
     return (
         <div className="menu-button">
             <Icon  className="icon"/>
-            <Link to={to} style={{color:"#13285E",textDecoration:"none",paddingLeft:"100px"}} >{title}</Link>
+            <Link to={to}  className="menu-link" >{title}</Link> 
         </div>
     )
 }
