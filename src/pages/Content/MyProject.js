31fd10bc6ff6075af4e5f68988d9dadc6b98ddcc
yeg2799 <yeg2799@gmail.com>
2021-03-19 02:27:18 +0300

    project page ui

--- src/pages/Content/MyProject.js
+++ src/pages/Content/MyProject.js
@@ -12,22 +12,31 @@ const MyProject = () => {
     <div className="my-project">
       <h1 className="my-project-title">Projelerim</h1>
       <div  className="my-project-row">
-      <Card className="my-project-card">
-      <CardTitle tag="h3" className="my-project-card-title">Card title</CardTitle>
+        
+        
+        <Card className="my-project-card">
+      <CardTitle tag="h3" className="my-project-card-title">My Website</CardTitle>
         <CardImg
           src={projectImage}
           alt="Card image cap"
           className="my-project-card-image"
         />
+        <a href="https://yeg27.netlify.app" target="_blank" className="project-link"><div className="projectCardButton">İncele</div></a>
       </Card>
-      <Card className="my-project-card">
-      <CardTitle tag="h3" className="my-project-card-title">Card title</CardTitle>
+
+       
+        
+        <Card className="my-project-card">
+      <CardTitle tag="h3" className="my-project-card-title">Covid-19 Tracker</CardTitle>
         <CardImg
           src={projectImage}
           alt="Card image cap"
           className="my-project-card-image"
         />
+        <a href="https://covd19-yeg.netlify.app/" target="_blank" className="project-link"><div className="projectCardButton">İncele</div></a>
       </Card>
+       
+    
       </div>
       
     </div>
