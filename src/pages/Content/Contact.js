d9802b694bb78de5b67e05c1eb7d7bea56ad6637
yeg2799 <yeg2799@gmail.com>
2021-03-19 04:36:12 +0300

    contact ui fix

--- src/pages/Content/Contact.js
+++ src/pages/Content/Contact.js
@@ -5,23 +5,23 @@ import {GrMail} from 'react-icons/gr'
 import '../../style/Content/contact.scss'
 const Contact = () => {
     return (
-        <div style={{paddingTop:"250px"}}>
+        <div  className="contact">
            <h1 style={{textAlign:"center"}}>İletişim</h1> 
-           <Form style={{width:"50%",margin:"50px auto"}}>
-      <Row form >
-        <Col md={6}>
+           <Form  className="form">
+      <div form className="inputs">
+        <div  className="inputName">
           <FormGroup className="inputWithIcon">
             <Input type="text"  placeholder="Adınız" />
               <BsFillPersonFill className="icon"/>
           </FormGroup>
-        </Col>
-        <Col md={6}>
+        </div>
+        <div className="inputSurname">
           <FormGroup className="inputWithIcon">
             <Input type="text"  placeholder="Soyadınız" />
             <BsFillPersonFill className="icon"/>
           </FormGroup>
-        </Col>
-      </Row>
+        </div>
+      </div>
       <FormGroup  className="inputWithIcon">
         <Input type="text"  placeholder="Email"/>
         <GrMail className="icon"/>
@@ -29,8 +29,6 @@ const Contact = () => {
       <FormGroup>
         <Input type="textarea"  placeholder="Adresinizi giriniz" style={{height:"200px"}}/>
       </FormGroup>
-     
-     
       <Button style={{width:"100%",background:"#E48686",border:"0px",color:"#000",fontSize:"20px"}}>Gönder</Button>
     </Form>
         </div>
